6bc7b1c468f6e1c16153f6ce390bd7165c20d9f3
Andrew67 <andres@andrew67.com>
2018-06-12 01:32:22 -0400

    Bump SW for deployment of v1.1

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,7 @@
 /*! DDR Calc | https://github.com/Andrew67/ddr-calc */
 
 // Bump this number to force a refresh of the static assets, by having it be considered a new SW version
-var swVersion = 1;
+var swVersion = 2;
 
 // See: https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook/
 self.addEventListener('install', function(event) {
