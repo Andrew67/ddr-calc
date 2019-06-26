25890834014d209b23599210d83a451d85da9758
Andrew67 <andres@andrew67.com>
2019-06-26 00:32:26 -0400

    Use OS/browser-reported dark mode when available instead of the toggle

--- src/js/dark.js
+++ src/js/dark.js
@@ -1,25 +1,37 @@
 /*! DDR Calc | https://github.com/Andrew67/ddr-calc */
-/* Contains functionality for showing a menu item that toggles and saves the dark mode preference */
+/* Contains functionality for toggling dark mode either through OS reporting or menu item */
 'use strict';
 
 // Dark mode CSS is contained in calc.css so that it loads as quickly as possible for flicker-free UI on return
 // Note: this module depends on the menu module loading first
 try {
-    // Dark mode toggle
-    // NOTE: any changes to this behavior need to be mirrored in the synchronous flicker-free block in index.html
-    const KEY_DARKMODE = 'dark-mode';
     dom.themeColor = document.querySelector('meta[name=theme-color]');
-    state.darkModeEnabled = localStorage.getItem(KEY_DARKMODE) === 'true';
 
-    addMenuItem(5, 'Dark Theme On/Off', () => {
-        state.darkModeEnabled = !state.darkModeEnabled;
-        try {
-            localStorage.setItem(KEY_DARKMODE, state.darkModeEnabled);
-        } catch (e) { /* Silently fail on exception (namely Safari in private browsing mode) */ }
-        commit();
-    }, {
-        title: () => state.darkModeEnabled ? 'Light Theme' : 'Dark Theme'
-    });
+    // Dark mode toggle for browsers that don't report dark mode
+    // NOTE: any changes to this behavior need to be mirrored in the synchronous flicker-free block in index.html
+    if (!window.matchMedia('(prefers-color-scheme)').matches) {
+        const KEY_DARKMODE = 'dark-mode';
+        state.darkModeEnabled = localStorage.getItem(KEY_DARKMODE) === 'true';
+
+        addMenuItem(5, 'Dark Theme On/Off', () => {
+            state.darkModeEnabled = !state.darkModeEnabled;
+            try {
+                localStorage.setItem(KEY_DARKMODE, state.darkModeEnabled);
+            } catch (e) { /* Silently fail on exception (namely Safari in private browsing mode) */ }
+            commit();
+        }, {
+            title: () => state.darkModeEnabled ? 'Light Theme' : 'Dark Theme'
+        });
+    } else {
+        // Toggle based on browser/OS reporting of dark mode
+        const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+        state.darkModeEnabled = darkModeQuery.matches;
+        darkModeQuery.addEventListener('change', evt => {
+            state.darkModeEnabled = evt.matches;
+            commit();
+        });
+    }
+
     postCommitHooks.push(function toggleDarkMode () {
         document.body.classList.toggle('theme-dark', state.darkModeEnabled);
         dom.themeColor.content = state.darkModeEnabled ? '#000000' : '#ffffff';
