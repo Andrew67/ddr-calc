3d53e662bbb296e8a59565ce7db5e6584e49feb0
Andrew67 <andres@andrew67.com>
2019-07-31 23:50:10 -0400

    Version bump to 3.2.0 in About screen Using logo with no shape around it for About screen

--- src/js/menu.js
+++ src/js/menu.js
@@ -113,8 +113,8 @@ fetch('img/md-more_vert.svg')
             '<div class="scrim"></div>' +
             '<div id="about-box">' +
                 '<div id="app-logo">' +
-                    '<img src="img/icon-192.png" width="64" height="64" alt="App Icon">' +
-                    '<h1>DDR Calc <span id="app-version">Version 3.1.2' +
+                    '<img src="img/logo-192.png" width="64" height="64" alt="App Icon">' +
+                    '<h1>DDR Calc <span id="app-version">Version 3.2.0' +
                         (isGPlay ? 'g' : '') + (isMobileSafari ? 's' : '') + '</span></h1>' +
                     '<h2>&copy; 2018&ndash;2019 Andrés Cordero</h2>' +
                 '</div>' +
