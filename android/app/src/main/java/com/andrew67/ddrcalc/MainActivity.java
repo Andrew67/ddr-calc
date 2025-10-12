7127c230e13d910b7203dfac2df1491fa1100716
Andrew67 <andres@andrew67.com>
2025-10-12 00:04:12 -0400

    Status bar light/dark fix. Version number correction. Portrait mode orientation. Step 1 of splash screen/icon customization.

--- android/app/src/main/java/com/andrew67/ddrcalc/MainActivity.java
+++ android/app/src/main/java/com/andrew67/ddrcalc/MainActivity.java
@@ -1,5 +1,41 @@
+/*
+ * Copyright (c) 2025 Andrés Cordero
+ * Web: https://github.com/Andrew67/ddr-calc
+ * LICENSE: GPLv3
+ *     https://github.com/Andrew67/ddr-calc/blob/master/LICENSE
+ */
+
 package com.andrew67.ddrcalc;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+import androidx.core.view.WindowInsetsControllerCompat;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        var windowInsetsController =
+                new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
+        windowInsetsController.setAppearanceLightStatusBars(
+                (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK)
+                        != Configuration.UI_MODE_NIGHT_YES
+        );
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        var windowInsetsController =
+                new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
+        windowInsetsController.setAppearanceLightStatusBars(
+                (newConfig.uiMode & Configuration.UI_MODE_NIGHT_MASK) !=
+                        Configuration.UI_MODE_NIGHT_YES
+        );
+    }
+
+}
